f3db5582321636da0448ead232c2af08ae86c20d
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2023-11-14 12:32:38 +0100

    update

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -8,7 +8,7 @@ public class movement : MonoBehaviour
     //var
     float maxXValue = 8.6f;
     float maxYValue = 4.7f;
-    int speed = 5;
+    public float speed = 5;
 
     // Start is called before the first frame update
     void Start()
