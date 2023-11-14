4107c372bcd4cec890c14b9f20b90a397499edb7
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-14 16:31:42 +0100

    heeeeelp

--- Assets/Scripts/mainCharacter/turnAroundPistol.cs
+++ Assets/Scripts/mainCharacter/turnAroundPistol.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class turnAroundPistol : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 point = new Vector3(5, 0, 0);
-        Vector3 axis = new Vector3(0, 0, 1);
-        transform.RotateAround(point, axis, Time.deltaTime * 10);
+        
     }
 
     // Update is called once per frame
