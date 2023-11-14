3e381ffa5c87761cf65b6abc0781f7f228785a12
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-14 12:28:12 +0100

    borders added

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -6,8 +6,8 @@ public class movement : MonoBehaviour
 {
 
     //var
-    int maxValue;
-    int minValue;
+    float maxXValue = 8.6f;
+    float maxYValue = 4.7f;
     int speed = 5;
 
     // Start is called before the first frame update
@@ -19,21 +19,21 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) /*&& transform.position.y < maxValue*/)
+        if (Input.GetKey(KeyCode.W) && transform.position.y < maxYValue)
         {
 
             transform.Translate(Vector3.up * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.S) /*&& transform.position.y > -maxValue*/)
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -maxYValue)
         {
 
             transform.Translate(Vector3.down * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.D) /*&& transform.position.y < minValue*/)
+        if (Input.GetKey(KeyCode.D) && transform.position.x < maxXValue)
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.A) /*&& transform.position.y > maxValue*/)
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -maxXValue)
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
