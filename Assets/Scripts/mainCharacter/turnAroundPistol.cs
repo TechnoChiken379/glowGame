557d2077cb7f6666ae472f7c3f5c31abfde46358
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-14 16:51:11 +0100

    horyee

--- Assets/Scripts/mainCharacter/turnAroundPistol.cs
+++ Assets/Scripts/mainCharacter/turnAroundPistol.cs
@@ -6,11 +6,12 @@ using UnityEngine.Animations;
 
 public class turnAroundPistol : MonoBehaviour
 {
-    
+    private float rotation;
     // Start is called before the first frame update
     void Start()
     {
-        
+       rotation = Mathf.Clamp(rotation, 0f, 180f);
+       transform.localRotation = Quaternion.Euler(180f, 0f, 0f);
     }
 
     // Update is called once per frame
