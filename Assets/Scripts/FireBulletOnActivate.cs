7f43df78f80ad8bf089eb7dd134281bcba9a5133
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2023-11-14 11:50:46 +0100

    Update FireBulletOnActivate.cs

--- Assets/Scripts/FireBulletOnActivate.cs
+++ Assets/Scripts/FireBulletOnActivate.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
-
 public class FireBulletOnActivate : MonoBehaviour
 {
     public GameObject bullet;
@@ -11,8 +9,7 @@ public class FireBulletOnActivate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
-        grabbable.activated.AddListener(FireBullet);
+   
     }
 
     // Update is called once per frame
