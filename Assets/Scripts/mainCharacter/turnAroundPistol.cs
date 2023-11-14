8159363bcf099fde8056809d9f7b89dc442b144a
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-14 16:11:29 +0100

    help

--- Assets/Scripts/mainCharacter/turnAroundPistol.cs
+++ Assets/Scripts/mainCharacter/turnAroundPistol.cs
@@ -7,7 +7,9 @@ public class turnAroundPistol : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.rotation = Quaternion.AngleAxis(90, Vector3.up);
+        Vector3 point = new Vector3(5, 0, 0);
+        Vector3 axis = new Vector3(0, 0, 1);
+        transform.RotateAround(point, axis, Time.deltaTime * 10);
     }
 
     // Update is called once per frame
