434b20f4767445739efdaa2901a71888f0d90968
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-14 15:49:02 +0100

    tryin sm

--- Assets/Scripts/mainCharacter/lookAtMe.cs
+++ Assets/Scripts/mainCharacter/lookAtMe.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class lookAtMe : MonoBehaviour
 {
+    //var
+    public GameObject target;
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
+        
+
 
     // Update is called once per frame
     void Update()
@@ -19,5 +24,7 @@ public class lookAtMe : MonoBehaviour
         var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
     }
 }
