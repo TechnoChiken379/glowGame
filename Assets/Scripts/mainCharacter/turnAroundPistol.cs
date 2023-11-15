768e4a2681ab897456cf15d794c18c80810f4ff8
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-15 12:08:44 +0100

    trying to make the thing turn around

--- Assets/Scripts/mainCharacter/turnAroundPistol.cs
+++ Assets/Scripts/mainCharacter/turnAroundPistol.cs
@@ -7,16 +7,27 @@ using UnityEngine.Animations;
 public class turnAroundPistol : MonoBehaviour
 {
     private float rotation;
+    public GameObject cube;
+    public GameObject pistol;
     // Start is called before the first frame update
     void Start()
     {
-       rotation = Mathf.Clamp(rotation, 0f, 180f);
-       transform.localRotation = Quaternion.Euler(180f, 0f, 0f);
+        rotation = Mathf.Clamp(rotation, 0f, 180f);
+        //rotation = Mathf.Clamp(rotation, 0f, 180f);
+        //transform.localRotation = Quaternion.Euler(180f, 0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < )
+        {
+        transform.localRotation = Quaternion.Euler(180f, 0f, 0f);
+        }
+        else if (transform.position.x > )
+        {
+        transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+
     }
 }
