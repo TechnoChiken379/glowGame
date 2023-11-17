cf0e6b5837f2a885cf3a9b503ae2c7df77b59abd
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2023-11-17 12:23:02 +0100

    push

---
+++ Assets/damadgeCollision.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damadgeCollision : MonoBehaviour
+{
+    double damadge = 0.15;
+
+    void BulletCollision(Collision collision)
+    {
+
+
+    }
+
+
+
+
+}
