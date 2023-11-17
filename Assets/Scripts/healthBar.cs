582b571f71f5a4c1df887736cebf5985ed158fba
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-17 12:47:16 +0100

    HP

--- Assets/Scripts/healthBar.cs
+++ Assets/Scripts/healthBar.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class healthBar : MonoBehaviour
 {
-    public float HBXPosition;
-    public float HBYPosition;
-    public float HBZPosition;
+    //public float HBXPosition;
+    //public float HBYPosition;
+    //public float HBZPosition;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(HBXPosition, HBYPosition, HBZPosition);
-        HBXPosition = 3.338341f;
-        HBYPosition = 7f;
-        HBZPosition = 4.3258f;
+        //transform.position = new Vector3(HBXPosition, HBYPosition, HBZPosition);
+        //HBXPosition = 3.338341f;
+        //HBYPosition = 7f;
+        //HBZPosition = 4.3258f;
     }
 
     // Update is called once per frame
