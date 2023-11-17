8bb711c12f2ce9bc4da75a326e67d06e20da54d9
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2023-11-17 12:39:22 +0100

    update

--- Assets/damadgeCollision.cs
+++ Assets/damadgeCollision.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class damadgeCollision : MonoBehaviour
-{
-    double damadge = 0.15;
+//public class damadgeCollision : MonoBehaviour
+//{
+//    double damadge = 0.15;
 
-    void BulletCollision(Collision collision)
-    {
+//    void BulletCollision(Collision collision)
+//    {
+//        if (collision.gameObject.CompareTag("enemy"))
+//        {
+//            InflictDamage(collision.gameObject);
+//        }
+
+//    }
+
+//    void 
 
 
-    }
 
 
-
-
-}
+//}
