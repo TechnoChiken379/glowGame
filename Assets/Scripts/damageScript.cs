da78e708e1a04a274763e8d4621a2d7c23098658
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2023-11-20 16:19:57 +0100

    .

--- Assets/Scripts/damageScript.cs
+++ Assets/Scripts/damageScript.cs
@@ -7,7 +7,7 @@ public class damageScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Damage();
+        
     }
 
     // Update is called once per frame
@@ -16,17 +16,15 @@ public class damageScript : MonoBehaviour
         
     }
 
-    void Damage(Collision collision)
+    void damage(Collision collision)
     {
         if (collision.gameObject.CompareTag("enemy"))
         {
-            Debug.Log("This Happend");
-            grunt.gruntHP = - 1;
+            grunt.gruntHP =- 1;
         }
-        if (grunt.gruntHP == 0)
+        if (1>2)
         {
-            Destroy(gameObject);
-            Debug.Log("This Happend as well");
+
         }
     }
 
--- Assets/Scripts/mainCharacter/movement.cs
+++ Assets/Scripts/mainCharacter/movement.cs
@@ -26,6 +26,13 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
+=======
+        //isMovingUp = false;
+        //isMovingRight = false;
+        //isMovingDown = false;
+        //isMovingLeft = false;
+>>>>>>> Stashed changes
         Move();
     }
 
