6b8f2c937202e154baca7df18bec6035815d6ef3
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-20 10:59:10 +0100

    hp work

--- Assets/Scripts/healthBar.cs
+++ Assets/Scripts/healthBar.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class healthBar : MonoBehaviour
 {
-    public float HBXPosition;
-    public float HBYPosition;
-    public float HBZPosition;
+    public float xScale;
     // Start is called before the first frame update
     void Start()
     {
-        
+        xScale = 6f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale = new Vector3(xScale, 0.7f, 1f);
+        xScale = 0.06f * characterCube.HP;
     }
 }
