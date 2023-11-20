e4b7d46bd4f5c340505dbdd42700ee45ce1f4f54
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2023-11-20 16:08:19 +0100

    update pls fix

--- Assets/Scripts/damageScript.cs
+++ Assets/Scripts/damageScript.cs
@@ -7,7 +7,7 @@ public class damageScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Damage();
     }
 
     // Update is called once per frame
@@ -16,15 +16,17 @@ public class damageScript : MonoBehaviour
         
     }
 
-    void damage(Collision collision)
+    void Damage(Collision collision)
     {
         if (collision.gameObject.CompareTag("enemy"))
         {
-            grunt.gruntHP =- 1;
+            Debug.Log("This Happend");
+            grunt.gruntHP = - 1;
         }
-        if (1>2)
+        if (grunt.gruntHP == 0)
         {
-
+            Destroy(gameObject);
+            Debug.Log("This Happend as well");
         }
     }
 
