cc3b392a748e6ae24b8245b473f66190be4ec9d7
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-21 10:44:12 +0100

    ahhhh

--- Assets/Scripts/damageScript.cs
+++ Assets/Scripts/damageScript.cs
@@ -7,22 +7,31 @@ public class damageScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       // Damage();
     }
 
     // Update is called once per frame
     void Update()
-    {
+   {
         
-    }
+//    }
 
-    void damage(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("enemy"))
-        {
-            //grunt.gruntHP =- 1;
-        }
-    }
+//    void Damage(Collision collision)
+//    {
+//        if (collision.gameObject.CompareTag("enemy"))
+//        {
+//            //HEAD
+//            //grunt.gruntHP =- 1;
+////
+//            Debug.Log("This Happend");
+//            grunt.gruntHP = - 1;
+//        }
+//        if (grunt.gruntHP == 0)
+//        {
+//            Destroy(gameObject);
+//            Debug.Log("This Happend as well")// parent of da78e70 (.)
+//        }
+   }
 
 
 }
