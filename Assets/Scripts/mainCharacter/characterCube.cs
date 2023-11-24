c73ec8d37619b3afb8bc094f72589e7f7acf1ac1
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-24 09:48:25 +0100

    it was so simple
    :(  it was simple

--- Assets/Scripts/mainCharacter/characterCube.cs
+++ Assets/Scripts/mainCharacter/characterCube.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class characterCube : MonoBehaviour
 {
     //var
-    public static bool canMoveUp = true;
-    public static bool canMoveRight = true;
-    public static bool canMoveDown = true;
-    public static bool canMoveLeft = true;
+    //public static bool canMoveUp = true;
+    //public static bool canMoveRight = true;
+    //public static bool canMoveDown = true;
+    //public static bool canMoveLeft = true;
 
     public static float HP = 100f;
     
@@ -28,77 +28,4 @@ public class characterCube : MonoBehaviour
         }
 
     }
-
-    //disables movement options if it touches something
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Check if the collision is with an object
-        //problem with that is that if it is moving up and right and touches something above him (so up)
-        //right is also disabled
-        if (collision.gameObject.CompareTag("Obstacle") && movement.isMovingUp == true)
-        {
-            canMoveUp = false;
-            Debug.Log("Obstacle detected!");
-        }
-        if (collision.gameObject.CompareTag("Obstacle") && movement.isMovingRight == true)
-        {
-            canMoveRight = false;
-            Debug.Log("Obstacle detected!");
-        }
-        if (collision.gameObject.CompareTag("Obstacle") && movement.isMovingDown == true)
-        {
-            canMoveDown = false;
-            Debug.Log("Obstacle detected!");
-        }
-        if (collision.gameObject.CompareTag("Obstacle") && movement.isMovingLeft == true)
-        {
-            canMoveLeft = false;
-            Debug.Log("Obstacle detected!");
-        }
-
-//// this is supposed to see where the things the player touches are
-//// it did not work (it might be able too)
-        //ContactPoint2D contact = collision.contacts[0];
-        //Vector2 normal = contact.normal;
-
-        //if (normal == Vector2.up)
-        //{
-        //    canMoveUp = false;
-        //    Debug.Log("Obstacle detected!");
-        //}
-        //else if (normal == Vector2.down)
-        //{
-        //    canMoveDown = false;
-        //    Debug.Log("Obstacle detected!");
-        //}
-        //else if (normal == Vector2.right)
-        //{
-        //    canMoveRight = false;
-        //    Debug.Log("Obstacle detected!");
-        //}
-        //else if (normal == Vector2.left)
-        //{
-        //    canMoveLeft = false;
-        //    Debug.Log("Obstacle detected!");
-        //}
-    }
-
-    //if collision left then player can move everywhere again
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            //if the can move true are places better, then you shouldn't be able to move throw objects
-            canMoveUp = true;
-            canMoveRight = true;
-            canMoveDown = true;
-            canMoveLeft = true;
-        }
-        
-    }
-
-   
-
-
-
 }
--- Assets/Scripts/mainCharacter/movement.cs
+++ Assets/Scripts/mainCharacter/movement.cs
@@ -8,14 +8,16 @@ public class movement : MonoBehaviour
 {
 
     //var
-    float maxXValue = 8.6f;
-    float maxYValue = 4.7f;
+    //float maxXValue = 8.6f;
+    //float maxYValue = 4.7f;
     public float speed = 5;
+    private float moveX;
+    private float moveY;
 
-    public static bool isMovingUp = false;
-    public static bool isMovingRight = false;
-    public static bool isMovingDown = false;
-    public static bool isMovingLeft = false;
+    //public static bool isMovingUp = false;
+    //public static bool isMovingRight = false;
+    //public static bool isMovingDown = false;
+    //public static bool isMovingLeft = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,55 +26,11 @@ public class movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Move();
-    }
-
-    void Move()
-    {
-        //does movement
-        if (Input.GetKey(KeyCode.W) && transform.position.y < maxYValue && characterCube.canMoveUp == true)
-        {
-
-               transform.Translate(Vector3.up * speed * Time.deltaTime);
-               isMovingUp = true;
-        } else
-        {
-            isMovingUp = false;
-        }
-
-        if (Input.GetKey(KeyCode.S) && transform.position.y > -maxYValue && characterCube.canMoveDown == true)
-            {
-
-                transform.Translate(Vector3.down * speed * Time.deltaTime);
-                isMovingDown = true;
-            }
-        else
-        {
-            isMovingDown = false;
-        }
-
-
-        if (Input.GetKey(KeyCode.D) && transform.position.x < maxXValue && characterCube.canMoveRight == true)
-            {
-                transform.Translate(Vector3.right * speed * Time.deltaTime);
-                isMovingRight = true;
-            }
-        else
-        {
-            isMovingRight = false;
-        }
-
-
-        if (Input.GetKey(KeyCode.A) && transform.position.x > -maxXValue && characterCube.canMoveLeft == true)
-            {
-                transform.Translate(Vector3.left * speed * Time.deltaTime);
-                isMovingLeft = true;
-            }
-        else
-        {
-            isMovingLeft = false;
-        }
+        //Move();
+        moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        moveY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position += new Vector3(moveX, moveY, 0);
     }
 }
