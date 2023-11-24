be01a7cc2772d22af5731d4c6326242b6100091d
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-24 09:49:15 +0100

    avoiding errors

--- Assets/Scripts/damageScript.cs
+++ Assets/Scripts/damageScript.cs
@@ -1,36 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Burst.CompilerServices;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using Unity.Burst.CompilerServices;
+//using UnityEngine;
 
-public class damageScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+//public class damageScript : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
        
-    }
+//    }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("enemy"))
-        {
-            grunt.gruntHP -= 1;
-            Debug.Log("FirstStepWorks");
-        }
-        if (grunt.gruntHP == 0)
-        {
-            if (collision.tag.Equals("enemy"))
-            {
-                Debug.Log("WORKING");
-                Destroy(hit.collider.gameObject);
-            }
-        }
-    }
-}
+//    void OnCollisionEnter2D(Collision2D collision)
+//    {
+//        if (collision.gameObject.CompareTag("enemy"))
+//        {
+//            grunt.gruntHP -= 1;
+//            Debug.Log("FirstStepWorks");
+//        }
+//        if (grunt.gruntHP == 0)
+//        {
+//            if (collision.tag.Equals("enemy"))
+//            {
+//                Debug.Log("WORKING");
+//                Destroy(hit.collider.gameObject);
+//            }
+//        }
+//    }
+//}
