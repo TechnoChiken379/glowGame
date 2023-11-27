4d7a44b2e6dc1c56d81e95f94be5bf7be1e705e2
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-11-27 14:25:16 +0100

    timer

--- Assets/Scripts/damageScript.cs
+++ Assets/Scripts/damageScript.cs
@@ -1,40 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Burst.CompilerServices;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using Unity.Burst.CompilerServices;
+//using UnityEngine;
 
-public class damageScript : MonoBehaviour
-{
-    public grunt gruntHealth;
-    private GameObject enemy;
-    // Start is called before the first frame update
-    void Start()
-    {
+//public class damageScript : MonoBehaviour
+//{
+//    public grunt gruntHealth;
+//    private GameObject enemy;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
 
-    }
+//    }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        gruntHealth = GetComponent<grunt>();
-        if (collision.gameObject.CompareTag("enemy"))
-        {
-            grunt.gruntHP -= 1;
-            Debug.Log("FirstStepWorks");
-            enemy = GameObject.find //https://www.youtube.com/watch?v=anHxFtiVuiE
-        }
-        if (gruntHealth.GetComponent<gruntHP>())
-        {
-            if (collision.gameObject.CompareTag("enemy"))
-            {
-                Debug.Log("WORKING");
-                Destroy(collision.gameObject);
-            }
-        }
-    }
-}
+//    void OnCollisionEnter2D(Collision2D collision)
+//    {
+//        gruntHealth = GetComponent<grunt>();
+//        if (collision.gameObject.CompareTag("enemy"))
+//        {
+//            grunt.gruntHP -= 1;
+//            Debug.Log("FirstStepWorks");
+//            enemy = GameObject.find //https://www.youtube.com/watch?v=anHxFtiVuiE
+//        }
+//        if (gruntHealth.GetComponent<gruntHP>())
+//        {
+//            if (collision.gameObject.CompareTag("enemy"))
+//            {
+//                Debug.Log("WORKING");
+//                Destroy(collision.gameObject);
+//            }
+//        }
+//    }
+//}
--- Assets/Scripts/mainCharacter/FireBulletOnActivate.cs
+++ Assets/Scripts/mainCharacter/FireBulletOnActivate.cs
@@ -9,6 +9,9 @@ public class FireBulletOnActivate : MonoBehaviour
     public GameObject bullet;
     public Transform bulletSpawnPoint;
     public float fireSpeed = 2500;
+
+    public float timer = 0f;
+    public float canFire = 0.25f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,18 +21,20 @@ public class FireBulletOnActivate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
         FireBullet();
     }
 
     public void FireBullet()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && timer >= canFire)
         {
             Debug.Log("Yes");
             GameObject spawnedBullet = Instantiate(bullet);
             spawnedBullet.transform.position = bulletSpawnPoint.position;
-            spawnedBullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.right * fireSpeed * Time.deltaTime;
+            spawnedBullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.right * fireSpeed/* * Time.deltaTime*/;
             Destroy(spawnedBullet, 2);
+            timer = 0f;
         }
         
     }
