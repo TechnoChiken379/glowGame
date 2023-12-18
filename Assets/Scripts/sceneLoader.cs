a0e44b58769e3e7f8644d44add8bd3e25d8344cc
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2023-12-18 14:02:16 +0100

    UPDATE

--- Assets/Scripts/sceneLoader.cs
+++ Assets/Scripts/sceneLoader.cs
@@ -9,4 +9,8 @@ public class sceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
