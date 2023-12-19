ddf0db2416871aef3498f776b870aaafb47c641e
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-12-19 11:56:43 +0100

    yo enemy spawn now

---
+++ Assets/Scripts/ememys/grunt/gurntSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+public class gurntSpawner : MonoBehaviour
+{
+
+    public GameObject grunt;
+    private float timer;
+    private float canSpawn = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= canSpawn)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject spawnedGrunt = Instantiate(grunt, transform.position, transform.rotation);
+    }
+}
