a99f5d7609ebf6310301621a331fa2af862d0789
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2023-12-21 11:42:05 +0100

    push

--- Assets/Scripts/mainCharacter/FireBulletOnActivate.cs
+++ Assets/Scripts/mainCharacter/FireBulletOnActivate.cs
@@ -6,12 +6,24 @@ using UnityEngine.UIElements;
 
 public class FireBulletOnActivate : MonoBehaviour
 {
-    public GameObject bullet;
+    public characterCube characterCube;
     public Transform bulletSpawnPoint;
-    public float fireSpeed = 12;
 
+    public GameObject bullet;
+    public float fireSpeed = 12;
     public float timer = 0f;
     public float canFire = 0.25f;
+
+    public GameObject sniperBullet;
+    public float sniperFireSpeed = 12;
+    public float sniperTimer = 0f;
+    public float sniperCanFire = 0.25f;
+
+    public GameObject shotGunBullet;
+    public float shotGunFireSpeed = 12;
+    public float shotGunTimer = 0f;
+    public float shotGunCanFire = 0.25f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +34,21 @@ public class FireBulletOnActivate : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        FireBullet();
+        if (characterCube.hotKey1 == true)
+        {
+            FirePistol();
+        }
+        if (characterCube.hotKey2 == true)
+        {
+            FireShotGun();
+        }
+        if (characterCube.hotKey3 == true)
+        {
+            FireSniper();
+        }
     }
 
-    public void FireBullet()
+    public void FirePistol()
     {
         if (Input.GetMouseButton(0) && timer >= canFire)
         {
@@ -34,6 +57,27 @@ public class FireBulletOnActivate : MonoBehaviour
             Destroy(spawnedBullet, 2);
             timer = 0f;
         }
-        
+    }
+
+    public void FireShotGun()
+    {
+        if (Input.GetMouseButton(0) && timer >= canFire)
+        {
+            GameObject spawnedBullet = Instantiate(shotGunBullet, bulletSpawnPoint.position, Quaternion.identity);
+            spawnedBullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.right * fireSpeed;
+            Destroy(spawnedBullet, 2);
+            timer = 0f;
+        }
+    }
+
+    public void FireSniper()
+    {
+        if (Input.GetMouseButton(0) && timer >= canFire)
+        {
+            GameObject spawnedBullet = Instantiate(sniperBullet, bulletSpawnPoint.position, Quaternion.identity);
+            spawnedBullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.right * fireSpeed;
+            Destroy(spawnedBullet, 2);
+            timer = 0f;
+        }
     }
 }
--- Assets/Scripts/mainCharacter/characterCube.cs
+++ Assets/Scripts/mainCharacter/characterCube.cs
@@ -10,17 +10,33 @@ public class characterCube : MonoBehaviour
 
     public static float HP = 100f;
     public static float backUpHP;
+
+    public static bool hotKey1 = false;
+    public static bool hotKey2 = false;
+    public static bool hotKey3 = false;
+
+    public GameObject pistol;
+    public GameObject shotGun;
+    public GameObject sniper;
     
     // Start is called before the first frame update
     void Start()
     {
         backUpHP = HP;
+
+        hotKey1 = true;
+        hotKey2 = false;
+        hotKey3 = false;
+
+        pistol.SetActive(true);
+        shotGun.SetActive(false);
+        sniper.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-    //tracts HP 
+        //tracts HP 
         if (HP < 1)
         {
             SceneManager.LoadScene("deathScreen");
@@ -37,6 +53,36 @@ public class characterCube : MonoBehaviour
             if (bossBuildIndex == 3) SceneManager.LoadScene("Boss 4");
             if (bossBuildIndex == 4) SceneManager.LoadScene("Boss 5");
             if (bossBuildIndex == 5) SceneManager.LoadScene("Boss 6");
+        }
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            hotKey1 = true;
+            hotKey2 = false;
+            hotKey3 = false;
+
+            pistol.SetActive(true);
+            shotGun.SetActive(false);
+            sniper.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            hotKey1 = false;
+            hotKey2 = true;
+            hotKey3 = false;
+
+            pistol.SetActive(false);
+            shotGun.SetActive(true);
+            sniper.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            hotKey1 = false;
+            hotKey2 = false;
+            hotKey3 = true;
+
+            pistol.SetActive(false);
+            shotGun.SetActive(false);
+            sniper.SetActive(true);
         }
     }
 }
