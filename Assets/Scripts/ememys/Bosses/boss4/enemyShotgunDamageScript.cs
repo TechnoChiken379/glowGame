222abf05192cd261d028e33ba85115e8768e0ca4
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-12-21 13:43:10 +0100

    ahhh

---
+++ Assets/Scripts/ememys/Bosses/boss4/enemyShotgunDamageScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyShotgunDamageScript : MonoBehaviour
+{
+    private grunt gruntHealth;
+    private GameObject enemy;
+
+    public static float damageAmount = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("mainCharacter"))
+        {
+            Destroy(transform.gameObject);
+            Debug.Log("Obstacle detected!");
+            characterCube.HP -= 2;
+        }
+        //    if (grunt.gruntHP == 0)
+        //    {
+        //        Debug.Log("SecondStepWorks");
+        //        Destroy(gameObject);
+        //    }
+    }
+
+
+}
+
